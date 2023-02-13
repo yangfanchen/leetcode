1f53cc9a47b21e1b90b66ccf303e2c30e8c51fbd
yangfanchen <cyf1442052750@gmail.com>
2023-02-12 22:49:52 -0400

    tower of hanoi code added

---
+++ src/recursion/TowerOfHanoi.java
@@ -0,0 +1,12 @@
+package recursion;
+
+public class TowerOfHanoi {
+	void TowersOfHanoi(int n, char frompeg,char topeg, char auxpeg) {
+		if(n==1) {
+			System.out.println("Move disk 1 from peg"+frompeg+" to peg" +topeg);
+		}
+		TowersOfHanoi(n-1,frompeg,auxpeg,topeg);
+		System.out.println("Move disk from peg"+frompeg+" to peg"+topeg);
+		TowersOfHanoi(n-1,auxpeg,topeg,frompeg);
+	}
+}
